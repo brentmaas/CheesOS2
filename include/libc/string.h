480f83fdc812eaf643ffcac777d5316e4d630131
Robin Voetter <robin@voetter.nl>
2020-02-06 13:00:20 +0100

    string.h mem functions

--- include/libc/string.h
+++ include/libc/string.h
@@ -5,4 +5,10 @@
 
 size_t strlen(const char*);
 
+void* memset(void* dest, int ch, size_t count);
+void* memcpy(void* restrict dest, const void* restrict src, size_t count);
+void* memmove(void* dest, const void* src, size_t count);
+int memcmp(const void* lhs, const void* rhs, size_t count);
+void* memchr(const void* ptr, int ch, size_t count);
+
 #endif
