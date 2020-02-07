48d8f6cd0caf19df4b0caa83814872365c3afe9b
Robin Voetter <robin@voetter.nl>
2020-02-07 11:35:23 +0100

    Remove kvm from makefile

--- include/core/io.h
+++ include/core/io.h
@@ -3,14 +3,14 @@
 
 #include <stdint.h>
 
-#define IO_MAKE_ROUTINE(io_name, io_type) \
-    static inline io_type io_in_##io_name(uint16_t port) { \
+#define IO_MAKE_ROUTINE(suffix, io_type) \
+    static inline io_type io_in##suffix(uint16_t port) { \
         io_type value; \
         asm volatile("in %[value], %[port]" : [value] "=a" (value) : [port] "Nd" (port)); \
         return value; \
     } \
     \
-    static inline void io_out_##io_name(uint16_t port, io_type value) { \
+    static inline void io_out##suffix(uint16_t port, io_type value) { \
         asm volatile("out %[port], %[value]" : : [value] "a" (value), [port] "Nd" (port)); \
     }
 
