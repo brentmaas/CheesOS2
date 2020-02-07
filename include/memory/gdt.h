3811b2019f76b180b5d768ed606bfedd5624f1da
Marcel <marcelhuijben@gmail.com>
2020-02-07 20:50:58 +0100

    GDT flag fix

--- include/memory/gdt.h
+++ include/memory/gdt.h
@@ -19,22 +19,25 @@ typedef struct  __attribute__((packed)) {
 } gdt_entry;
 
 typedef enum {
-    GDT_ACCESS_ACCESSED = 1,
-    GDT_ACCESS_RW = 2,
-    GDT_ACCESS_DIRECT_CONFIRM = 4,
-    GDT_ACCESS_EXECUTE = 8,
-    GDT_ACCESS_SYSTEM = 16,
-    GDT_ACCESS_PRIVILEGE = 32 | 64,
-    GDT_ACCESS_PRIVILEGE_0 = 0,
-    GDT_ACCESS_PRIVILEGE_1 = 32,
-    GDT_ACCESS_PRIVILEGE_2 = 64,
-    GDT_ACCESS_PRIVILEGE_3 = 32 | 64,
-    GDT_ACCESS_PRESENT = 128
+    GDT_ACCESS_ACCESSED = 1 << 0,
+    GDT_ACCESS_RW = 1 << 1,
+    GDT_ACCESS_DIRECT_CONFIRM = 1 << 2,
+    GDT_ACCESS_EXECUTE = 1 << 3,
+    GDT_ACCESS_SYSTEM = 1 << 4,
+    GDT_ACCESS_PRIVILEGE = (1 << 5) | (1 << 6),
+    GDT_ACCESS_PRESENT = 1 << 7
 } gdt_access_type;
 
+#define GDT_ACCESS_PRIVILEGE_0 (0u)
+#define GDT_ACCESS_PRIVILEGE_1 (1u << 5u)
+#define GDT_ACCESS_PRIVILEGE_2 (1u << 6u)
+#define GDT_ACCESS_PRIVILEGE_3 ((1u << 5u) | (1u << 6u))
+
 typedef enum {
-    GDT_FLAG_SIZE = 2,
-    GDT_FLAG_GRANULARITY = 4
+    GDT_FLAG_AVL = 1 << 0,
+    GDT_FLAG_LONG_MODE = 1 << 1,
+    GDT_FLAG_SIZE = 1 << 2,
+    GDT_FLAG_GRANULARITY = 1 << 3
 } gdt_flags_type;
 
 void gdt_init(void);
--- src/core/entry.c
+++ src/core/entry.c
@@ -8,8 +8,6 @@
 
 void kernel_main(multiboot_info* multiboot) {
     vga_init();
-    vga_print("Test1");
-
     gdt_init();
     // idt_init();
 
