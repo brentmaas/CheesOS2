4dbbb82277edf277f5318b2da6bbe9013507db9d
BIsHetAntwoord <34161672+BIsHetAntwoord@users.noreply.github.com>
2020-06-24 23:17:13 +0200

    strncmp fix

--- src/libc/string/string.c
+++ src/libc/string/string.c
@@ -18,8 +18,10 @@ int strcmp(const char* lhs, const char* rhs){
 }
 
 int strncmp(const char* lhs, const char* rhs, size_t count){
+	if(count == 0)
+            return 0;
 	size_t i = 0;
-	while(lhs[i] > 0 && lhs[i] == rhs[i] && i < count - 1){
+	while(lhs[i] && lhs[i] == rhs[i] && i < count){
 		++i;
 	}
 	return (int) lhs[i] - (int) rhs[i];
